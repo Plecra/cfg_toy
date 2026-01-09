984b702fd7ca41124a55de89b22a3c015e0363c3
Plecra <marli@frost.red>
2026-01-09 08:24:00 +0000

    and pull out the completions module

---
+++ src/completions.rs
@@ -0,0 +1,62 @@
+use super::{State, Completion, NtSymbol};
+/// Semantically, this is a `BTreeMap<(usize, NtSymbol), State<'a>>`
+/// It's implemented via a flat buffer containing all the entries in correct order,
+/// and the completion index for locating each value of `usize`. This
+/// provides range queries for `(i, sym)` efficiently.
+pub struct Completions<'a> {
+    completions: Vec<Completion<'a>>,
+    completion_index: Vec<usize>,
+}
+impl<'a> Completions<'a> {
+    pub fn new(len: usize) -> Self {
+        let completions = vec![];
+        let mut completion_index = Vec::with_capacity(len + 1);
+        completion_index.push(0);
+        Self { completions, completion_index }
+    }
+    pub fn add_group(&mut self) -> CompletionsTransaction<'a, '_> {
+        CompletionsTransaction::new(self)
+    }
+    pub fn query(&self, back_ref: usize, sym: NtSymbol) -> impl Iterator<Item = State<'a>> + '_ {
+        let start = self.completion_index[back_ref];
+        let end = self.completion_index[back_ref + 1];
+        let start_of_comps = start + self.completions[start..end].partition_point(|c| c.0 < sym);
+        let end_of_comps = start + self.completions[start..end].partition_point(|c| c.0 <= sym);
+        self.completions[start_of_comps..end_of_comps]
+            .iter()
+            .map(|c| c.1)
+    }
+}
+// Appending a new group to the Completions buffer needs to be careful to
+// clean up once it's done, which this type is responsible for.
+pub struct CompletionsTransaction<'a, 'b> {
+    completions: &'b mut Completions<'a>,
+    start_len: usize,
+}
+impl<'a, 'b> CompletionsTransaction<'a, 'b> {
+    fn new(completions: &'b mut Completions<'a>) -> Self {
+        let start_len = completions.completions.len();
+        Self {
+            completions,
+            start_len,
+        }
+    }
+    pub fn query(&self, back_ref: usize, sym: NtSymbol) -> impl Iterator<Item = State<'a>> + '_ {
+        self.completions.query(back_ref, sym)
+    }
+    pub fn push(&mut self, completion: Completion<'a>) {
+        self.completions.completions.push(completion);
+    }
+}
+impl<'a, 'b> Drop for CompletionsTransaction<'a, 'b> {
+    fn drop(&mut self) {
+        // let end_len = self.completions.completions.len();
+        // self.completions
+        //     .completion_index
+        //     .push(end_len);
+        self.completions.completions[self.start_len..].sort();
+        self.completions
+            .completion_index
+            .push(self.completions.completions.len());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod completions;
+use completions::{Completions, CompletionsTransaction};
 #[derive(Debug)]
 struct Rule {
     for_nt: u32,
@@ -108,61 +110,6 @@ impl<'a, 'b> StateGrouping<'b> for FromOldStates<'a, 'b> {
     }
 }
 
-// Semantically, this is a `BTreeMap<(usize, NtSymbol), State<'a>>`
-// It's implemented via a flat buffer containing all the entries in correct order,
-// and the completion index for locating each value of `usize`. This
-// provides range queries for `(i, sym)` efficiently.
-struct Completions<'a> {
-    completions: Vec<Completion<'a>>,
-    completion_index: Vec<usize>,
-}
-impl<'a> Completions<'a> {
-    fn add_group(&mut self) -> CompletionsTransaction<'a, '_> {
-        CompletionsTransaction::new(self)
-    }
-    fn query(&self, back_ref: usize, sym: NtSymbol) -> impl Iterator<Item = State<'a>> + '_ {
-        let start = self.completion_index[back_ref];
-        let end = self.completion_index[back_ref + 1];
-        let start_of_comps = start + self.completions[start..end].partition_point(|c| c.0 < sym);
-        let end_of_comps = start + self.completions[start..end].partition_point(|c| c.0 <= sym);
-        self.completions[start_of_comps..end_of_comps]
-            .iter()
-            .map(|c| c.1)
-    }
-}
-// Appending a new group to the Completions buffer needs to be careful to
-// clean up once it's done, which this type is responsible for.
-struct CompletionsTransaction<'a, 'b> {
-    completions: &'b mut Completions<'a>,
-    start_len: usize,
-}
-impl<'a, 'b> CompletionsTransaction<'a, 'b> {
-    fn new(completions: &'b mut Completions<'a>) -> Self {
-        let start_len = completions.completions.len();
-        Self {
-            completions,
-            start_len,
-        }
-    }
-    fn query(&self, back_ref: usize, sym: NtSymbol) -> impl Iterator<Item = State<'a>> + '_ {
-        self.completions.query(back_ref, sym)
-    }
-    fn push(&mut self, completion: Completion<'a>) {
-        self.completions.completions.push(completion);
-    }
-}
-impl<'a, 'b> Drop for CompletionsTransaction<'a, 'b> {
-    fn drop(&mut self) {
-        // let end_len = self.completions.completions.len();
-        // self.completions
-        //     .completion_index
-        //     .push(end_len);
-        self.completions.completions[self.start_len..].sort();
-        self.completions
-            .completion_index
-            .push(self.completions.completions.len());
-    }
-}
 fn parse_earley(cfg: &Cfg, src: &[u8], init_sym: u32) -> Ast {
     let mut states = cfg
         .rules_for(init_sym)
@@ -176,11 +123,7 @@ fn parse_earley(cfg: &Cfg, src: &[u8], init_sym: u32) -> Ast {
     // let mut completions: Vec<Completion> = vec![];
     // let mut completion_index: Vec<usize> = Vec::with_capacity(src.len());
     // completion_index.push(0);
-    let mut completions = Completions {
-        completions: vec![],
-        completion_index: Vec::with_capacity(src.len()),
-    };
-    completions.completion_index.push(0);
+    let mut completions = Completions::new(src.len());
 
     for cursor in 0..src.len() {
         println!("@{cursor} {states:?}");
