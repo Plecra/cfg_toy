512620487a082dbb6f50c066490dc194c2878d48
Plecra <marli@frost.red>
2026-01-09 11:41:56 +0000

    test ambiguity

--- tests/grammar_examples.rs
+++ tests/grammar_examples.rs
@@ -1,7 +1,7 @@
 #[test]
 fn simple_logic() {
     let mut mycfg = cfg_toy::cfg! {
-        expr and_expr primary alpha ident ws gap and or not;
+        expr and_expr primary alpha ident ws gap and or not ambiguous_1 ambiguous_2;
 
         ws ::= " " .
         ws ::= "\n" .
@@ -18,7 +18,13 @@ fn simple_logic() {
         or ::= gap "or" gap.
         not ::= "not" gap.
 
+        // Ambiguous rules to test parse forest handling
+        ambiguous_1 ::= "then".
+        ambiguous_2 ::= "theatre".
+
         expr ::= and_expr or expr.
+        expr ::= and_expr gap ambiguous_1.
+        expr ::= and_expr gap ambiguous_2.
         expr ::= and_expr.
         and_expr ::= primary and and_expr.
         and_expr ::= primary.
@@ -31,4 +37,5 @@ fn simple_logic() {
     println!("{:?}", mycfg);
     mycfg.rules.sort_by_key(|rule| rule.for_nt);
     let _ = cfg_toy::parse_earley(&mycfg, "true or false and not false or bb".as_bytes(), 256);
+    let _ = cfg_toy::parse_earley(&mycfg, "true then".as_bytes(), 256);
 }
